2e195fd6a103c315d8e6eb24e3a3a4fba53b41c4
Gabriel <hernandezgabrie671@gmail.com>
2021-11-01 11:32:55 -0400

    creando ttt

---
+++ infinity-bot/commands/ttt.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js")
+const math = require("mathjs")
+
+module.exports = {
+	name: "calculator",
+	description: "Habilita una calculadora",
+	alias: ["calc"],
+	type: "utility",
+	async execute({ message, embed, args }) {
+
+		const member = essage.mentions.members.first();
+		const time = 1000 * 60 * 10
+		let button = new Array([], [], []);
+		let row = [];
+		let current = 0;
+
+		const addRows = (btns) => {
+			let row1 = new Discord.MessageActionRow();
+
+			for(const btn of btns){
+				row1.addComponents(btn);
+			}
+
+			return row1;
+		};
+
+
+		const createButton = (label,style = "SECONDARY") => {
+			const btn = new Discord.MessageButton()
+			.setLabel(label)
+			.setStyle(style)
+			.setCustomId("ttt" + label)
+
+			return btn;
+		}
+
+		for(let i = 0; i < 10; i++){
+			if(button[current].length === 3) current++;
+			button[current].push(createButton(text[i]))
+			if(i === 9){
+				for(let btn of button) row.push(addRows(btn));
+			}
+		}
+
+		embed.setTitle("Tic Tac Toe");
+		embed.sedDescription(`Jugadores: \n\n${message.author}:x: \n\n${member}`);
+		message.channel.send({embeds:[embed], components: row})
+
+
+
+	},
+};
